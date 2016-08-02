78e3fa16399a10a6581ff0be2671b869120ce0d4
Bianca <bianca@jawsdigital.com.br>
2016-08-02 13:00:19 -0300

    ARRAY

--- phps.php
+++ phps.php
@@ -5,18 +5,30 @@
 </head>
 <body>
 	<?php
+		$txt = "Hello World!";
+		$txt2 = "Bianca";
+		echo "$txt $txt2";
 		$x = 5;
 		$y = 10;
-		function myTeste{
-			$txt = "Hello World!";
-			$txt2 = "Bianca";
-			echo "$txt $txt2";
-		}
 		function myTest() {
-   			$GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
-		}
+    		$GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
+		} 
 		myTest();
-		echo "$y"; // outputs 15
+		echo $y; // outputs 15
+
+		print "<h2>PHP is Fun!</h2>";
+		print "Hello world!<br>";
+		print "I'm about to learn PHP!";
+		echo "<h2>PHP is Fun!</h2>";
+		echo "Hello world!<br>";
+		echo "I'm about to learn PHP!";
+		$x = 10.365;
+		var_dump($x);
+		$cars = array("Volvo","BMW","Toyota");
+		$i;
+		//for(i=0;i<3;i++){
+			echo "$cars";
+		//}
 	?>
 </body>
 </html>
